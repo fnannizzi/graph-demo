62277619ae8fed65f4b1c580db1c2c2f522c99e1
Francesca Nannizzi <fnannizzi@gmail.com>
2016-01-09 18:38:00 -0500

    Testing

--- scripts/fb_login.js
+++ scripts/fb_login.js
@@ -76,7 +76,7 @@ window.fbAsyncInit = function() {
   function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
-      console.log('Successful login for: ' + response.name);
+      console.log('Successful login for: ' + response.name + ' ID: ' response.userID);
       document.getElementById('status').innerHTML =
         'Thanks for logging in, ' + response.name + '!';
     });
