0fa7f89146fa8c2ef1c0e14be7702a689b1d7014
Francesca Nannizzi <fnannizzi@gmail.com>
2016-01-10 21:44:59 -0500

    Testing

--- scripts/fb_login.js
+++ scripts/fb_login.js
@@ -73,11 +73,10 @@ window.fbAsyncInit = function() {
 
   // Here we run a very simple test of the Graph API after login is
   // successful.  See statusChangeCallback() for when this call is made.
-  function testAPI() {
+  /*function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
       console.log('Successful login for: ' + response.name + ' ID: ' response.id);
-      document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response.name + '!';
+      document.getElementById('status').innerHTML = ('Thanks for logging in, ' + response.name + '!');
     });
-  }
+  }*/
