d898a4e903a290f491f4fd0ecdbc12b31b7d86fd
Francesca Nannizzi <fnannizzi@gmail.com>
2016-01-11 00:45:51 -0500

    Adding more friendlist logic

--- scripts/build_graph.js
+++ scripts/build_graph.js
@@ -1,9 +1,35 @@
-// Here we run a very simple test of the Graph API after login is
-// successful.  See statusChangeCallback() for when this call is made.
+function friendlist(id, name, members) {
+	this.ID = id;
+    this.name = name;
+	if (members === undefined) {
+      this.members = []
+	}
+	else {
+      this.members = members;
+    }
+}
+
 function fetchFriendLists() {
-  console.log('Welcome!  Fetching your information.... ');
   FB.api('/me/friendlists', function(response) {
-  	console.log(response);
-    console.log('Successful friendlist grab');
+  	if (response && !response.error)  {
+	  console.log('Successful friendlist grab');
+	  console.log(response);
+	  var friendlists = [];
+	  for (index = 0; index < response.data.length; index++) {
+	  	var newFriendlist = new friendlist(response.data[index].id, getFriendListName(response.data[index].id));
+	  	friendLists.push(newFriendlist);
+	  }
+	  console.log(friendlists)
+  	}
   });
+}
+
+function getFriendListName(id) {
+  var name;
+  FB.api('/id', function(response) {
+  	console.log('Successfully fetched friendlist');
+  	console.log(response);
+  	name = response.name;
+  });
+  return name;
 }
