c85cb194ae8b41fcafa5f800a2778de57977f2c4
Francesca Nannizzi <fnannizzi@gmail.com>
2016-01-10 22:01:40 -0500

    Testing

--- scripts/fb_login.js
+++ scripts/fb_login.js
@@ -69,3 +69,14 @@ window.fbAsyncInit = function() {
   js.src = "https://connect.facebook.net/en_US/sdk.js";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
+
+
+  // Here we run a very simple test of the Graph API after login is
+  // successful.  See statusChangeCallback() for when this call is made.
+  function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Successful login for: ' + response.name + ' ID: ' response.id);
+      document.getElementById('status').innerHTML = ('Thanks for logging in, ' + response.name + '!');
+    });
+  }
