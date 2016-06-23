f620008371ff370a6588101127fb029e721225d1
Francesca Nannizzi <fnannizzi@gmail.com>
2016-06-22 23:15:55 -0400

    Revert "Quick refactoring of createGraph.js"
    This reverts commit 34aab752b0b57a9d30db312b8f14d397f2dae1b2.

--- scripts/createGraph.js
+++ scripts/createGraph.js
@@ -1,18 +1,22 @@
-function createGraph(graph, numNodes) {
-  console.log('Creating graph');
+function createAndShowGraph(numNodes) {
   var graph = new Graph();
-  useWeightedEdges(graph);
-  addEdgesToGraph(graph);
+  createGraph(graph, numNodes);
+  var renderer = drawGraph(graph);
 
-  return graph;
+  findShortestPathAndAnimate(graph, renderer, numNodes);
+
+  //colorGraph(graph, renderer, numNodes);
 }
 
-function addEdgesToGraph(graph, numNodes) {
+function createGraph(graph, numNodes) {
+  console.log('Creating graph');
+  addWeightedEdges(graph);
+
   // Create an adjacency matrix to store already created edges, so we don't repeat them
   adjacencyMatrix = [];
 
   for (nodeIndex = 0; nodeIndex < numNodes; nodeIndex++) {
-    graph.addNode(convertIndexToLetter(nodeIndex));
+  	graph.addNode(convertIndexToLetter(nodeIndex));
     console.log('Added node ' + convertIndexToLetter(nodeIndex))
 
     // Add a new row to the adjacency list
@@ -23,12 +27,22 @@ function addEdgesToGraph(graph, numNodes) {
   for (nodeIndex = 0; nodeIndex < numNodes; nodeIndex++) {
     // Generate a random number of edges
     numEdges = Math.floor(Math.random() * Math.ceil(numNodes/3) + 1);
+    console.log("NUMEDGES: " + numEdges);
     for (edgeIndex = 0; edgeIndex < numEdges; edgeIndex++) {
       var otherNode = chooseNode(nodeIndex, numNodes);
       
       // If the edge doesn't already exist, create it
       if (!checkForExistingEdge(adjacencyMatrix, nodeIndex, otherNode)) {
-        addEdgeToGraph(graph, nodeIndex, otherNode);        
+        var nodeName = convertIndexToLetter(nodeIndex);
+        var otherNodeName = convertIndexToLetter(otherNode);
+
+        // flip a coin to decide which direction the edge goes
+        if (coinFlip()) { // heads goes away
+          graph.addEdge(nodeName, otherNodeName);
+        }
+        else { // tails goes towards
+          graph.addEdge(otherNodeName, nodeName);
+        }
 
         // Update the adjacency matrix
         adjacencyMatrix[nodeIndex][otherNode] = 1;
@@ -47,21 +61,22 @@ function checkForExistingEdge(adjacencyMatrix, nodeA, nodeB) {
   }
 }
 
-function addEdgeToGraph(graph, nodeIndex, otherNode) {
-  var nodeName = convertIndexToLetter(nodeIndex);
-  var otherNodeName = convertIndexToLetter(otherNode);
+function drawGraph(graph) {
+  // layout the graph using the Spring layout implementation
+  var layouter = new Graph.Layout.Spring(graph);
+  layouter.layout();
+   
+  // draw the graph using the RaphaelJS draw implementation
+  var width = document.getElementById("canvas").offsetWidth - 30;
+  var height = 400;
+  var renderer = new Graph.Renderer.Raphael('canvas', graph, width, height);
+  renderer.draw();
 
-  // flip a coin to decide which direction the edge goes
-  if (coinFlip()) { // heads goes away
-    graph.addEdge(nodeName, otherNodeName);
-  }
-  else { // tails goes towards
-    graph.addEdge(otherNodeName, nodeName);
-  }
+  return renderer;
 }
 
 // Borrowed from https://github.com/strathausen/dracula/blob/master/examples/algorithms.js
-function useWeightedEdges(graph) {
+function addWeightedEdges(graph) {
   /* modify the addEdge function to attach random weights */
   graph.addEdgeToGraph = graph.addEdge;
   graph.addEdge = function(from, to, style) {
--- scripts/findShortestPath.js
+++ scripts/findShortestPath.js
@@ -1,8 +1,18 @@
 function findShortestPathAndAnimate(graph, renderer, numNodes) {
-  renderer.draw();
   dijkstra(graph, graph.nodes.A);
-  colorGraph(graph, renderer, numNodes);
-//  animateGraph(graph, renderer, graph.nodes.A);
+
+
+  graph.edges.forEach(function(e) {
+    if(e.target.predecessor === e.source || e.source.predecessor === e.target)
+    {
+      e.style.stroke = '#bfa';
+      e.style.fill   = '#56f';
+    } else {
+      e.style.stroke = '#aaa';
+    }
+  });
+
+  renderer.draw();
 
 }
 
@@ -19,24 +29,68 @@ function colorGraph(graph, renderer, numNodes) {
   }
 }
 
-function animateGraph(graph, renderer, node) {
-  //setTimeout(function(graph, node) {
-    console.log("drawing");
-    node.shape[0].animate({fill:'green'},200);
-    /*
-    graph.edges.forEach(function(e) {
-      if(e.target.predecessor === e.source || e.source.predecessor === e.target) {
-        e.style.stroke = '#bfa';
-        e.style.fill   = '#56f';
-      } 
-      else {
-        e.style.stroke = '#aaa';
-      }*/
-    //});
-    //}(graph, node), 1000);
+/*
+   Credit to the Dracula source code, from which I have heavily borrowed.  
+ 
+   Path-finding algorithm Dijkstra
+   
+   - worst-case running time is O((|E| + |V|) · log |V| ) thus better than
+     Bellman-Ford for sparse graphs (with less edges), but cannot handle
+     negative edge weights
+ */
+function dijkstra(g, source) {
 
-    renderer.draw();
-    console.log("done drawing");
+    /* initially, all distances are infinite and all predecessors are null */
+    for(var n in g.nodes)
+        g.nodes[n].distance = Infinity;
+        /* predecessors are implicitly null */
+
+    g.snapShot("Initially, all distances are infinite and all predecessors are null.");
+
+    source.distance = 0;
+    /* set of unoptimized nodes, sorted by their distance (but a Fibonacci heap
+       would be better) */
+    var q = new BinaryMinHeap(g.nodes, "distance");
+
+    /* pointer to the node in focus */
+    var node;
+
+    /* get the node with the smallest distance
+       as long as we have unoptimized nodes. q.min() can have O(log n). */
+    while(q.min() != undefined) {
+        /* remove the latest */
+        node = q.extractMin();
+        node.optimized = true;
+
+        /* no nodes accessible from this one, should not happen */
+        if(node.distance == Infinity)
+            throw "Orphaned node!";
+
+        /* for each neighbour of node */
+        for(e in node.edges) {
+      var other = (node == node.edges[e].target) ? node.edges[e].source : node.edges[e].target;
+    
+            if(other.optimized)
+                continue;
+
+            /* look for an alternative route */
+            var alt = node.distance + node.edges[e].weight;
+            
+            /* update distance and route if a better one has been found */
+            if (alt < other.distance) {
+            
+                /* update distance of neighbour */
+                other.distance = alt;
+
+                /* update priority queue */
+                q.heapify();
+
+                /* update path */
+                other.predecessor = node;
+                g.snapShot("Enhancing node.")
+            }
+        }
+    }
 }
 
 
